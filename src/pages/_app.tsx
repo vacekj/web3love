56253429339600c4593fa70233c671e4d1de420c
Josef Vacek <vacekj@outlook.com>
2022-02-13 13:17:04 -0700

    add moralis sdk with hardcoded env vars

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import { AppProps } from 'next/app';
-
-import '@/styles/globals.css';
+import { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MoralisProvider
+      appId="KQWivZ5GLgs4hqVJbrBmwECgm4Zv5O4ifgCBMjst"
+      serverUrl="https://5u4vllqzdjdb.usemoralis.com:2053/server"
+    >
+      <Component {...pageProps} />;
+    </MoralisProvider>
+  );
 }
 
 export default MyApp;
