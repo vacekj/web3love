2693cd7a42c1e51059fd8d9ec3601d0b26ffae0f
Jan Ruettinger <service+github@janruettinger.com>
2022-02-13 12:18:24 -0700

    chore: delete unncessary files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import Seo from '@/components/Seo';
-
 /**
  * SVGR Support
  * Caveat: No React Props Type.
@@ -17,30 +10,24 @@ import Seo from '@/components/Seo';
 
 export default function HomePage() {
   return (
-    <Layout>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
-
-      <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <div>Connect</div>
-
+    <main>
+      <section className='bg-white'>
+        <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
+          <div className='rounded-md bg-red-300 p-4'>Connect</div>
+          <div>
+            <div>To</div>
             <div>
-              <div>To</div>
-              <div>
-                <input type='addres' />
-              </div>
-              <div>Message</div>
-              <input type='message' />
-              <div>Canvas drawing</div>
+              <input type='addres' />
             </div>
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{" Jessi's hackers"}
-            </footer>
+            <div>Message</div>
+            <input type='message' />
+            <div>Canvas drawing</div>
           </div>
-        </section>
-      </main>
-    </Layout>
+          <footer className='absolute bottom-2 text-gray-700'>
+            © {new Date().getFullYear()} By{" Jessi's hackers"}
+          </footer>
+        </div>
+      </section>
+    </main>
   );
 }
