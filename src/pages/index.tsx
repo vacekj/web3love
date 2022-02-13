93b3ec89eb9a210918c79a99d71defcb0a8bc2a9
Jan Ruettinger <service+github@janruettinger.com>
2022-02-13 12:52:33 -0700

    wip: add canvas drawing + ui progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import CanvasDraw from 'react-canvas-draw';
 
 /**
  * SVGR Support
@@ -15,23 +16,30 @@ export default function HomePage() {
         <div className='mt-16 rounded-md bg-orange-100 py-8 px-4 text-2xl'>
           Welcome to Web3 Love
         </div>
-        <div className='mt-8 rounded-md bg-orange-100 p-4'>Connect</div>
+        <div className='mt-8 rounded-md bg-orange-100 p-4 text-2xl'>
+          Connect
+        </div>
         <div className='mx-auto mt-8 flex w-4/5 flex-col justify-start rounded-md bg-white p-4'>
           <div className='text-left'>To:</div>
-          <div className='text-left'>
-            <input
-              type='addres'
-              className='bg-orange-300'
-              placeholder='Enter the recipient address here'
-            />
-          </div>
-          <div>Message:</div>
+          <input
+            type='addres'
+            className='rounded-md bg-orange-300 p-2'
+            placeholder='Enter the recipient address here'
+          />
+          <div className='mt-4 text-left'>Message:</div>
           <input
             type='message'
-            className='bg-orange-300'
+            className='h-32 rounded-md bg-orange-300 p-2'
             placeholder='Enter the message here'
           />
-          <div>Canvas drawing</div>
+          <div className='w-full'>
+            <CanvasDraw hideGrid={true} canvasHeight={400} canvasWidth={800} />
+          </div>
+          <div className='flex justify-center'>
+            <div className='w-fit rounded-md bg-red-700 p-4 text-2xl text-white'>
+              <button>Send</button>
+            </div>
+          </div>
         </div>
         <footer className='absolute bottom-2 text-gray-700'>
           © {new Date().getFullYear()} By{" Jessi's hackers"}
