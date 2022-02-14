6a56b8837c0c045213695d81d700209a2a1920b7
Jan Ruettinger <service+github@janruettinger.com>
2022-02-14 01:08:27 -0700

    feat: add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import SendMessage from '@/components/SendMessage';
 import abi from '../nftContractAbi';
 import CanvasDraw from 'react-canvas-draw';
 import ResetCanvasButton from '@/components/ResetCanvasButton';
+import Seo from '@/components/Seo';
+import Layout from '@/components/layout/Layout';
 
 export default function HomePage() {
   const canvas = useRef<HTMLCanvasElement | null>(null);
@@ -62,69 +64,72 @@ export default function HomePage() {
   const [recipient, setRecipient] = useState('');
 
   return (
-    <main className=''>
-      <div className='layout flex min-h-screen flex-col items-center text-center'>
-        <div className='mt-16 rounded-md bg-orange-100 py-8 px-4 text-2xl'>
-          Welcome to Web3 Love
-        </div>
-        <div>{user?.get('ethAddress')}</div>
-        <ConnectButton />
+    <Layout>
+      <Seo />
+      <main className=''>
+        <div className='layout flex min-h-screen flex-col items-center text-center'>
+          <div className='mt-16 rounded-md bg-orange-100 py-8 px-4 text-2xl'>
+            Welcome to Web3 Love
+          </div>
+          <div>{user?.get('ethAddress')}</div>
+          <ConnectButton />
 
-        <div className='mx-auto mt-8 flex flex-col justify-start rounded-md'>
-          <div className='relative mt-8 w-full'>
-            <CanvasDraw
-              hideGrid={true}
-              canvasHeight={697}
-              canvasWidth={1024}
-              imgSrc={'/images/envelope_background.jpg'}
-              backgroundImage={'/images/envelope_background.jpg'}
-              ref={canvas}
-              className={'bg-transparent'}
-            />
-
-            <div className='absolute top-16 right-24'>
-              <img src='/images/envelope_stamp.png' alt='' />
-            </div>
-
-            <div className='absolute top-32 left-32 rounded-md border-2 border-black bg-transparent'>
-              <textarea
-                onChange={(e) => {
-                  if (canvas.current) {
-                    const ctx = canvas.current.ctx.drawing;
-                    ctx.clearRect(100, 100, 500, 500);
-                    ctx.font = '24px serif';
-                    wrapText(ctx, e.target.value, 150, 170, 400, 24);
-                    // ctx.fillText(e.target.value, 150, 150);
-                    setMessage(e.target.value);
-                  }
-                }}
-                id=''
-                name=''
-                rows={6}
-                cols={30}
-                className='bg-transparent text-2xl opacity-0'
+          <div className='mx-auto mt-8 flex flex-col justify-start rounded-md'>
+            <div className='relative mt-8 w-full'>
+              <CanvasDraw
+                hideGrid={true}
+                canvasHeight={697}
+                canvasWidth={1024}
+                imgSrc={'/images/envelope_background.jpg'}
+                backgroundImage={'/images/envelope_background.jpg'}
+                ref={canvas}
+                className={'bg-transparent'}
               />
+
+              <div className='absolute top-16 right-24'>
+                <img src='/images/envelope_stamp.png' alt='' />
+              </div>
+
+              <div className='absolute top-32 left-32 rounded-md border-2 border-black bg-transparent'>
+                <textarea
+                  onChange={(e) => {
+                    if (canvas.current) {
+                      const ctx = canvas.current.ctx.drawing;
+                      ctx.clearRect(100, 100, 500, 500);
+                      ctx.font = '24px serif';
+                      wrapText(ctx, e.target.value, 150, 170, 400, 24);
+                      // ctx.fillText(e.target.value, 150, 150);
+                      setMessage(e.target.value);
+                    }
+                  }}
+                  id=''
+                  name=''
+                  rows={6}
+                  cols={30}
+                  className='bg-transparent text-2xl opacity-0'
+                />
+              </div>
+            </div>
+            <div className='mt-4 flex flex-row justify-around'>
+              <div className='flex justify-center'>
+                <ResetCanvasButton onClick={onClickReset} />
+              </div>
+              <div className='flex justify-center'>
+                <SendMessage onClick={onClickSend} />
+              </div>
+            </div>
+            <div>
+              <div className='mt-8 text-left text-2xl text-white'>
+                Your messages:
+              </div>
             </div>
           </div>
-          <div className='mt-4 flex flex-row justify-around'>
-            <div className='flex justify-center'>
-              <ResetCanvasButton onClick={onClickReset} />
-            </div>
-            <div className='flex justify-center'>
-              <SendMessage onClick={onClickSend} />
-            </div>
-          </div>
-          <div>
-            <div className='mt-8 text-left text-2xl text-white'>
-              Your messages:
-            </div>
-          </div>
+          <footer className='m-4 text-2xl text-black'>
+            © {new Date().getFullYear()} By{" Jessi's hackers"}
+          </footer>
         </div>
-        <footer className='m-4 text-2xl text-black'>
-          © {new Date().getFullYear()} By{" Jessi's hackers"}
-        </footer>
-      </div>
-    </main>
+      </main>
+    </Layout>
   );
 }
 
