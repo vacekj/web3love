ac8453ba0bdaddc7444f35c7f8addb948d4d78ee
Josef Vacek <vacekj@outlook.com>
2022-02-13 23:25:18 -0700

    fix nft metadat uploda

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,16 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { NFTStorage } from 'nft.storage';
+import { NFTStorage, File } from 'nft.storage';
+import { Blob } from 'buffer';
 
 const API_KEY = process.env.NEXT_NFT_STORAGE_API_KEY;
 const client = new NFTStorage({ token: API_KEY! });
 
 async function storeNft(
-  image: Blob,
+  image: string,
   recipientAddress: string,
   message: string
 ) {
+  const buff = Buffer.from(image, 'base64url');
+
   const nft = {
-    image,
+    image: new File([buff], recipientAddress + '.jpeg', {
+      type: 'image/jpeg',
+    }),
     name: 'Web3Love Letter',
     description:
       "Give the gift of web3 this Valentine's day to your favourite person",
@@ -26,20 +31,25 @@ async function storeNft(
 type Request = {
   recipient: string;
   message: string;
-  image: Blob;
+  image: string;
 };
 
 /* Takes an image and NFT metadata
  * and uploads both to IPFS via Infura
  * Returns an object with the NFT metadata that now includes the JPEG IPFS Link */
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body as Request;
+
   if (!body.recipient || !body.image || !body.message) {
     return res.status(400).json({
       error: 'Bad request',
     });
   }
 
-  const metadata = storeNft(body.image, body.recipient, body.message);
-  return res.status(201).json(metadata);
+  try {
+    const metadata = await storeNft(body.image, body.recipient, body.message);
+    return res.status(201).json(metadata);
+  } catch (e) {
+    return res.status(500).json(e);
+  }
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ConnectButton from '@/components/ConnectButton';
 import { useApiContract, useMoralis, useNFTBalances } from 'react-moralis';
 import SendMessage from '@/components/SendMessage';
 import abi from '../nftContractAbi';
 import CanvasDraw from 'react-canvas-draw';
+import { ReactSketchCanvas, ReactSketchCanvasRef } from 'react-sketch-canvas';
 
 export default function HomePage() {
-  const canvas = useRef();
+  const canvas = useRef<ReactSketchCanvasRef | null>(null);
   const { user } = useMoralis();
 
   const { getNFTBalances, data } = useNFTBalances({
@@ -16,17 +17,24 @@ export default function HomePage() {
   });
   useEffect(() => {
     getNFTBalances();
+    address.current = user?.get('ethAddress');
   }, [user]);
+  const address = useRef<string | null>();
 
   const { data: nftData, runContractFunction: mintNft } = useApiContract({
     address: '0x72B6Dc1003E154ac71c76D3795A3829CfD5e33b9',
     functionName: 'mint',
     abi,
-    params: {},
+    params: {
+      to: address.current,
+    },
   });
 
+  const [message, setMessage] = useState('');
+  const [recipient, setRecipient] = useState('');
+
   return (
-    <main className='bg-red-700'>
+    <main className='relative bg-red-700'>
       <div className='layout flex min-h-screen flex-col items-center text-center'>
         <div className='mt-16 rounded-md bg-orange-100 py-8 px-4 text-2xl'>
           Welcome to Web3 Love
@@ -37,49 +45,69 @@ export default function HomePage() {
         <div className='mx-auto mt-8 flex flex-col justify-start rounded-md bg-white p-4'>
           <div className='text-left'>To:</div>
           <input
-            type='addres'
+            onChange={(e) => {
+              setRecipient(e.target.value);
+            }}
+            value={recipient}
             className='rounded-md bg-orange-300 p-2'
             placeholder='Enter the recipient address here'
           />
           <div className='mt-4 text-left'>Message:</div>
           <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             type='message'
             className='h-32 rounded-md bg-orange-300 p-2'
             placeholder='Enter the message here'
           />
           <div className='mt-8 w-full'>
-            <CanvasDraw
-              hideGrid={true}
-              canvasHeight={697}
-              canvasWidth={1024}
-              imgSrc={'/images/envelope_background.jpg'}
+            <ReactSketchCanvas
+              height={'697'}
+              width={'1024'}
+              exportWithBackgroundImage={true}
+              strokeColor={''}
+              backgroundImage={'/images/envelope_background.jpg'}
               ref={canvas}
               className={'bg-contain'}
             />
           </div>
           <div className='flex justify-center'>
             <SendMessage
-              onClick={() => {
-                fetch('/api/upload', {
+              onClick={async () => {
+                const data = await canvas.current?.exportImage('jpeg');
+                console.log(data);
+                const nftMetadata = await fetch('/api/upload', {
                   method: 'POST',
                   body: JSON.stringify({
-                    recipient: '',
+                    recipient,
+                    image: data,
+                    message,
                   }),
-                });
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                }).then((r) => r.json());
+                console.log(nftMetadata);
               }}
             />
           </div>
           <div>
             Your messages:
             {data?.result?.map((r) => {
-              return <img className={'h-32 w-32'} src={r.metadata?.image} />;
+              return (
+                <img
+                  className={'h-32 w-32'}
+                  src={r.metadata?.image}
+                  alt={r.name}
+                />
+              );
             })}
           </div>
         </div>
-        <footer className='absolute bottom-2 text-gray-700'>
-          © {new Date().getFullYear()} By{" Jessi's hackers"}
-        </footer>
       </div>
+      <footer className='absolute bottom-2 text-gray-700'>
+        © {new Date().getFullYear()} By{" Jessi's hackers"}
+      </footer>
     </main>
   );
 }
