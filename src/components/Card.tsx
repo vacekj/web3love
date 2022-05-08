c676236bc4820b6bc1591dec7fde032fc7a3317d
Josef Vacek <vacekj@outlook.com>
2022-05-08 17:05:17 +0200

    feat(frontend): new frontend

---
+++ src/components/Card.tsx
@@ -0,0 +1,123 @@
+import nftContractAbi from "@/nftContractAbi.json";
+import abi from "@/nftContractAbi.json";
+import { Box, Button, Center, Heading, Image, Input, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import { ConnectButton, useAddRecentTransaction } from "@rainbow-me/rainbowkit";
+import { useState } from "react";
+import { useAccount, useContractWrite } from "wagmi";
+const IMAGE = "/images/mothers-day.jpeg";
+
+export default function Card() {
+  const addRecentTransaction = useAddRecentTransaction();
+  const [recipient, setRecipient] = useState<string>("0x70997970c51812dc3a010c7d01b50e0d17dc79c8");
+  const { data: account } = useAccount();
+  const { writeAsync, isLoading: mintLoading } = useContractWrite(
+    {
+      addressOrName: process.env.NEXT_PUBLIC_NFT_CONTRACT!,
+      contractInterface: abi.abi,
+    },
+    "safeMint",
+    {
+      args: [recipient, ""],
+    },
+  );
+
+  return (
+    <Center py={12} flexDirection={"column"}>
+      <Box
+        role={"group"}
+        p={6}
+        maxW={"330px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        pos={"relative"}
+        zIndex={1}
+        mb={8}
+      >
+        <Box
+          rounded={"lg"}
+          mt={-12}
+          pos={"relative"}
+          height={"230px"}
+          _after={{
+            transition: "all .3s ease",
+            content: "\"\"",
+            w: "full",
+            h: "full",
+            pos: "absolute",
+            top: 5,
+            left: 0,
+            backgroundImage: `url(${IMAGE})`,
+            filter: "blur(15px)",
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: "blur(20px)",
+            },
+          }}
+        >
+          <Image
+            rounded={"lg"}
+            height={230}
+            width={282}
+            objectFit={"cover"}
+            src={IMAGE}
+          />
+        </Box>
+        <Stack pt={10} align={"flex-start"} spacing={3}>
+          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+            from
+          </Text>
+          <Input size={"lg"} variant={"unstyled"} autoFocus={true} placeholder={"your loving son"} />
+
+          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+            to
+          </Text>
+          <Input size={"lg"} variant={"unstyled"} autoFocus={true} placeholder={"the best mother"} />
+
+          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+            send to
+          </Text>
+          <Input
+            required={true}
+            value={recipient}
+            onChange={e => setRecipient(e.target.value)}
+            size={"lg"}
+            variant={"unstyled"}
+            autoFocus={true}
+            placeholder={"0xa9fac1ba6c7fb0ffb44ecec01cf23d47bba924d25336defdbf782e7181fc00bd"}
+          />
+        </Stack>
+      </Box>
+      {<ConnectButton />}
+      {account && (
+        <Button
+          type={"submit"}
+          isLoading={mintLoading}
+          onClick={async () => {
+            const result = await writeAsync();
+            console.log(result);
+            addRecentTransaction({
+              hash: result.hash,
+              description: "Mint message NFT",
+            });
+          }}
+          size={"lg"}
+          px={8}
+          mt={4}
+          border={"solid 3px linear(to-r, red.400,pink.400)"}
+          colorScheme={"pink"}
+          bgGradient="linear(to-r, red.400,pink.400)"
+          textColor={"white"}
+          _hover={{
+            bgGradient: "linear(to-r, red.500,pink.500)",
+          }}
+        >
+          Send some love
+        </Button>
+      )}
+    </Center>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,30 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import "@rainbow-me/rainbowkit/styles.css";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { MoralisProvider } from "react-moralis";
 
+import { apiProvider, configureChains, getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, createClient, WagmiProvider } from "wagmi";
+
+const { provider, chains } = configureChains(
+  [chain.polygon, chain.localhost],
+  [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Web3Love",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <>
       <Head>
@@ -13,14 +34,18 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
       </Head>
-      <ChakraProvider>
-        <MoralisProvider
-          appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID!}
-          serverUrl={process.env.NEXT_PUBLIC_SERVER_URL!}
-        >
-          <Component {...pageProps} />
-        </MoralisProvider>
-      </ChakraProvider>
+      <WagmiProvider client={wagmiClient}>
+        <RainbowKitProvider chains={chains} showRecentTransactions={true}>
+          <ChakraProvider>
+            <MoralisProvider
+              appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID!}
+              serverUrl={process.env.NEXT_PUBLIC_SERVER_URL!}
+            >
+              <Component {...pageProps} />
+            </MoralisProvider>
+          </ChakraProvider>
+        </RainbowKitProvider>
+      </WagmiProvider>
     </>
   );
 }
